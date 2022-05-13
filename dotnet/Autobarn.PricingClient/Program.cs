d483e1663814d04ed7c76163b03269f2ded45d4b
Dylan Beattoie <dylan@dylanbeattie.net>
2022-05-13 13:57:27 +0100

    gRPC pricing client and server working in .NET

---
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Autobarn.Messages;
+using Autobarn.PricingEngine;
+using EasyNetQ;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace Autobarn.PricingClient {
+    internal class Program {
+        private static Pricer.PricerClient grpcClient;
+        private static readonly IConfigurationRoot config = ReadConfiguration();
+        static void Main(string[] args) {
+            using var channel = GrpcChannel.ForAddress(config.GetConnectionString("AutobarnPricingServerUrl"));
+            grpcClient = new Pricer.PricerClient(channel);
+            var amqp = config.GetConnectionString("AutobarnRabbitMqConnectionString");
+            using var bus = RabbitHutch.CreateBus(amqp);
+            bus.PubSub.Subscribe<NewVehicleMessage>("Autobarn.PricingClient",
+                HandleNewVehicleMessage,
+                x => x.WithAutoDelete());
+            Console.WriteLine("Listening for NewVehicleMessages - press Enter to quit");
+            Console.ReadLine();
+        }
+
+        private static async void HandleNewVehicleMessage(NewVehicleMessage m) {
+            Console.WriteLine($"retrieving price for {m.Registration} ({m.Make} {m.Model}, {m.Color}, {m.Year}");
+            var request = new PriceRequest {
+                Make = m.Make,
+                Color = m.Color,
+                Year = m.Year,
+                Model = m.Model
+            };
+            var priceReply = await grpcClient.GetPriceAsync(request);
+            Console.WriteLine($"Price: {priceReply.Price} {priceReply.CurrencyCode}");
+        }
+
+        private static IConfigurationRoot ReadConfiguration() {
+            var basePath = Directory.GetParent(AppContext.BaseDirectory).FullName;
+            return new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
---
+++ dotnet/Autobarn.PricingServer/Services/GreeterService.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Autobarn.PricingEngine;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Autobarn.PricingServer.Services {
+    public class PricerService : Pricer.PricerBase {
+        private readonly ILogger<PricerService> logger;
+        public PricerService(ILogger<PricerService> logger) {
+            this.logger = logger;
+        }
+
+        public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context) {
+            return Task.FromResult(new PriceReply {
+                Price = 5000,
+                CurrencyCode = "GBP"
+            });
+        }
+    }
+}
